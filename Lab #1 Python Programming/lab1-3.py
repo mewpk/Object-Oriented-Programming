cda7fc8eaab4f121ce53bd2c0eeafdc9e3b606ba
MewPK <milzaza123@gmail.com>
2023-01-12 22:00:27 +0700

    remove lib

--- Lab #1 Python Programming/lab1-3.py
+++ Lab #1 Python Programming/lab1-3.py
@@ -1,14 +1,15 @@
-from math import ceil
 def calculateTime(listNumber):
     listNumber[0] += listNumber[1]/60
     listNumber[2] += listNumber[3]/60
-    time = ceil(listNumber[2]-listNumber[0])
+    time = listNumber[2]-listNumber[0]
+    if type(time) is float : 
+        time += 1
     if time >= 6 :
         return 200
     elif time >= 4 :
-        return (time-3)*20 + 30
+        return round((time-3)*20 + 30)
     elif listNumber[2]*60 + listNumber[3] - listNumber[0]*60 + listNumber[1] > 15:
-        return time*10
+        return round(time*10)
     else :
         return 0
 print(calculateTime(list(map(int,input().split()))))
